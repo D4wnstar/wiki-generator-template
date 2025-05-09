00ad1713affddce5c3e29994d9261a9d2db6d6b3
D4wnstar <104584158+D4wnstar@users.noreply.github.com>
2025-05-09 19:54:22 +0200

    Added dark mode persistence

--- src/lib/components/utils/Lightswitch.svelte
+++ src/lib/components/utils/Lightswitch.svelte
@@ -2,24 +2,29 @@
 	import { Switch } from '@skeletonlabs/skeleton-svelte'
 	import IconMoon from 'lucide-svelte/icons/moon'
 	import IconSun from 'lucide-svelte/icons/sun'
+	import { onMount } from 'svelte'
 
-	// Bind to the checked state
-	let mode = $state(false)
+	let mode = $state(true)
 
 	// Handle the change in state when toggled.
-	function handleModeChange() {
-		// NOTE: implementation may differ per Tailwind config and framework:
-		// https://tailwindcss.com/docs/dark-mode#toggling-dark-mode-manually
-		document.documentElement.classList.toggle('dark')
+	function handleModeChange(newState: boolean) {
+		document.documentElement.classList.toggle('dark', newState)
+		localStorage.setItem('darkMode', String(newState))
 	}
+
+	onMount(() => {
+		// Initialize from localStorage (need browser so in onMount it goes)
+		mode = (localStorage.getItem('darkMode') ?? 'true') === 'true'
+	})
 </script>
 
 <Switch
 	name="mode"
-	controlActive="bg-surface-200"
-	bind:checked={mode}
-	onCheckedChange={handleModeChange}
+	controlActive="bg-surface-500"
+	controlInactive="bg-surface-200"
+	checked={mode}
+	onCheckedChange={(e) => handleModeChange(e.checked)}
 >
-	{#snippet inactiveChild()}<IconMoon size="14" />{/snippet}
-	{#snippet activeChild()}<IconSun size="14" />{/snippet}
+	{#snippet activeChild()}<IconMoon size="14" />{/snippet}
+	{#snippet inactiveChild()}<IconSun size="14" />{/snippet}
 </Switch>
